a45576810b62ad9fd452fde810d803b2adfadaf7
matezolt <101284249+matezolt@users.noreply.github.com>
2022-06-01 10:52:51 +0200

    Update CalendarSelect.java

--- src/CalendarSelect.java
+++ src/CalendarSelect.java
@@ -10,11 +10,29 @@ public class CalendarSelect {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Eszti\\eclipse\\chromedriver.exe");
 		//webdriver.chrome.driver -> value of the path
 		WebDriver driver = new ChromeDriver();  
-		driver.get("https://rahulshettyacademy.com/dropdownsPractise/"); //get site
 		
-		//Calendar
-		//		.ui-state-default ui-state-highlight
-		driver.findElement(By.cssSelector(".ui-state-default ui-state-highlight")).click();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.path2usa.com/travel-companions");
+		//April 14
+		driver.findElement(By.xpath(".//*[@id='travel_date']")).click();  //click calendar field
+		
+		//select months from parent
+		while(!driver.findElement(By.cssSelector("[class='datepicker-days'] [class='datepicker-switch']")).getText().contains("June")) {
+			
+			driver.findElement(By.cssSelector("[class='datepicker-days'] th[class='next']")).click();
+		}
+			
+		List<WebElement> days = driver.findElements(By.className("day"));
+		//grab common attribute // put into a list and iterate over
+		int count = driver.findElements(By.className("day")).size();
+		for(int i=0; i<count; i++) {
+			String text = driver.findElements(By.className("day")).get(i).getText();
+			if(text.equals("21")){
+				driver.findElements(By.className("day")).get(i).click();
+				break;
+			}
+		}
 		
 	}
 
